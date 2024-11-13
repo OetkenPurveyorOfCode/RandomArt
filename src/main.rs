c03698dc3ed6b85b428203e0d3c49fc533c4b32f
OetkenPurveyorOfCode <oetkenpurveyorofcode@gmail.com>
2024-11-13 02:41:54 +0100

    Color gradient image

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use image::{RgbImage, Rgb};
+
+struct Vector3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+struct Vector2 {
+    x: f32,
+    y: f32,
+}
+
+fn gradient(input: Vector2) -> Vector3 {
+    return Vector3{x: input.x, y: input.y, z: 0.0};
+}
+
+fn main() {
+    let height = 200;
+    let width = 300;
+    let mut img = RgbImage::new(width, height);
+    for y in 0..height {
+        for x in 0..width {
+            let v3 = gradient(Vector2{x: x as f32/width as f32, y: y as f32/height as f32});
+            img.put_pixel(x, y, Rgb([(v3.x*255.0) as u8, (v3.y*255.0) as u8, (v3.z*255.0) as u8]));
+        }
+    }
+    img.save("out.bmp");
+}
